6358f07eea7f7d336b411a8a08283079466158cf
Sree Vasanthan k <123319138+Toymaker0@users.noreply.github.com>
2024-07-11 17:57:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ import path from "path"
 const __dirname = path.resolve();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(cookie());
-
+app.use(cors(
+   {
+      origin:['http://localhost:3000'],credentials:true
+   }
+))
 // const db = mysql.createConnection({
 //   host: "localhost",
 //   user: "root",
